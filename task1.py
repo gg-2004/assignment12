9d3784c13e4fe907298de2b578a16de5cdb27bed
gg-2004 <138923116+gg-2004@users.noreply.github.com>
2025-06-27 12:38:10 +0530

    adding file

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition: ",c)
+print("subtraction: ",d)
+print("multiplication: ",e)
+print("division: ",f)
---
+++ task2.py
@@ -0,0 +1,4 @@
+a= input("enter the first name: ",)
+b= input("enter the last name: ",)
+c=a+b
+print(" hello ", c ,"welcome to the python program" )
